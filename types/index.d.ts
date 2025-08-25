f1e918f641583de650f35f933bdc6ff58452077a
Chloé Gerry <cgerry@corp.free.fr>
2025-08-25 17:50:15 +0200

    feat: add type file to avoid type error in package import

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export type DatePickerProps = {
+  minimumDate?: string;
+  maximumDate?: string;
+  id?: string;
+  containerClassName?: string;
+  calendarClassName?: string;
+};
+
+export declare const DatePicker: React.FC<DatePickerProps>;
+export default DatePicker;
